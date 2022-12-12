2ecebae387ccc431b3861ec47d7c376016d8f151
mariolopezp <101217777+mariolopezp@users.noreply.github.com>
2022-12-12 07:04:47 -0800

    pulseSensor v2.0 - Adaptada a programa principal
    Ahora la función que realiza las medidas contiene un bucle que dura 30 segundos y realiza las medidas cada 10 ms (el delay también está incluido dentro de esta función). La implementación general ha cambiado, no usandose variables privadas globales a todo el módulo, solo las variables propias de las funciones. La función que realiza la medida devuelve un 1 si se ha producido un error y un valor distinto de 1 si la medida ha sido correcta con el número de pulsaciones por minuto detectado

---
+++ Final_app/src/pulseSensor/hardware.h
@@ -0,0 +1,19 @@
+#ifndef hardware_h
+#define hardware_h
+
+  #define D0 16
+  #define D1 5
+  #define D2 4
+  #define D3 0
+  #define D4 2
+  #define D5 14
+  #define D6 12
+  #define D7 13
+  #define D8 15
+  
+  #define TX 1
+  #define RX 3
+  
+  #define A0 A0
+
+#endif
--- Final_app/src/pulseSensor/pulseSensor.cpp
+++ Final_app/src/pulseSensor/pulseSensor.cpp
@@ -12,19 +12,13 @@
 /* Includes ------------------------------------------------------------------*/
 #include "pulseSensor.h"             // Module header
 
-int periodo_muestreo  = 10;
-int duracion_medida   = 30000;
-int tiempo_set_up     = 10000;
-int tiempo_transitorio = 5000;
 
-int media;
-int contador_pulsos;
-int tension_entrada;
+//Si se quiere optimizar aún más se puede jugar con los múltiplos de TIEMPO_TRANSITORIO.
+ #define TIEMPO_TRANSITORIO  500
+ #define TIEMPO_ESTABLECIMIENTO_MEDIA  1000
+ #define TIEMPO_MEDIDA  3000
 
-float maximo;
 
-int heart_rate;
-bool primera_deteccion;
 
 /* Private typedef -----------------------------------------------------------*/
 
@@ -35,68 +29,62 @@ bool primera_deteccion;
 /* Functions -----------------------------------------------------------------*/
 
 
-void init_hr_sensor(int pin_alimentacion, bool init_medida, bool fin_medida, bool fin_transitorio){
+void init_hr_sensor(int pin_alimentacion){
   pinMode(pin_alimentacion,OUTPUT);
   pinMode(A0,INPUT);
   digitalWrite(pin_alimentacion, HIGH);
-  fin_medida = false;
-  init_medida = false;
-  contador_pulsos = 0;
-  media = 0;
-  tension_entrada = 0;
-  maximo = 0;
-  primera_deteccion = true;
-  fin_transitorio = false;
-  heart_rate = -1;
 }
 
 
-int realizar_medidas(bool init_medida, bool fin_medida, bool fin_transitorio){
-  if(fin_transitorio){
-    if(!fin_medida && init_medida){
-      tension_entrada = 600 + (analogRead(A0) - (media*0.002))*300 ; // +1000
-      if(tension_entrada > maximo){
-        maximo = tension_entrada; // valor cercano a 2200
-        //Serial.println(maximo);
-      }
-      Serial.println(maximo);
-      Serial.println(tension_entrada);
-      if(tension_entrada > maximo*0.60){
-        if(primera_deteccion){
-          contador_pulsos++;
-          Serial.println(contador_pulsos);
-          primera_deteccion = false;
+
+uint8_t realizar_medidas(){
+  int tension_entrada;
+  uint16_t contador_intervalo_muestreo = 0;
+  uint8_t contador_pulsos = 0;
+  uint16_t heart_rate = 0;
+  int media = 0;
+  float maximo = 0;
+  bool primera_deteccion = true;  
+  
+  while(contador_intervalo_muestreo <= TIEMPO_MEDIDA){
+    if(contador_intervalo_muestreo  >= TIEMPO_TRANSITORIO){
+      if(contador_intervalo_muestreo > TIEMPO_ESTABLECIMIENTO_MEDIA && contador_intervalo_muestreo < TIEMPO_MEDIDA){
+        tension_entrada = 600 + (analogRead(A0) - (media*0.002))*300 ; // +1000
+        if(tension_entrada > maximo){
+          maximo = tension_entrada; // valor cercano a 2200
         }
-      }else if(tension_entrada < maximo*0.25){ //Dejo aquí una tierra de nadie para las oscilaciones.
-        primera_deteccion = true;
-      }    
-    }else if(!fin_medida && !init_medida && fin_transitorio){
-      media = media + (analogRead(A0));
-      Serial.println(media);
+        
+        Serial.println(maximo);
+        Serial.println(tension_entrada);
+        if(tension_entrada > maximo*0.60){
+          if(primera_deteccion){
+            contador_pulsos++;
+            Serial.println(contador_pulsos);
+            primera_deteccion = false;
+          }
+          
+        }else if(tension_entrada < maximo*0.25){ //Dejo aquí una tierra de nadie para las oscilaciones.
+          primera_deteccion = true;
+        }
+            
+      }else if(contador_intervalo_muestreo > TIEMPO_TRANSITORIO && contador_intervalo_muestreo <= TIEMPO_ESTABLECIMIENTO_MEDIA){
+        media = media + (analogRead(A0));
+        Serial.println(media);
     
-    }else{
-      if(contador_pulsos*3 < 20 || contador_pulsos*3 > 200){ //Se ha producido un error en la medida.
-        heart_rate = 1;
-      }else{
-        heart_rate = contador_pulsos*3;                  //Medida correcta.
+      }else if (contador_intervalo_muestreo >= TIEMPO_MEDIDA){
+        if(contador_pulsos*3 < 30 || contador_pulsos*3 > 200){ //Se ha producido un error en la medida.
+          heart_rate = 1;
+        }else{
+          heart_rate = contador_pulsos*3;                  //Medida correcta.
+        }
       }
     }
+    contador_intervalo_muestreo++;
+    delay(10);
   }
   return heart_rate;
 }
 
-void switch_off_hr_sensor(int pin_alimentacion){
+void switch_off_hr_sensor(uint8_t pin_alimentacion){
   digitalWrite(pin_alimentacion, LOW);
 }
-/*
-void avisar_fin_transitorio(bool fin_transitorio){
-  fin_transitorio = true;
-}
-
-void deshabilitarMedida(bool fin_medida){
-  fin_medida = true;
-}
-
-void iniciar_Medida(bool init_medida){
-  init_medida = true;
-}*/
